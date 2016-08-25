5c61836dd66140d30c6a814f43e1f0ed5e8860cb
Kirill Sotnikov <wlan@cpan.org>
2016-08-25 10:33:38 +0300

    ELB health-check

--- src/aws_lambda_example/core.clj
+++ src/aws_lambda_example/core.clj
@@ -1,6 +1,7 @@
 (ns aws-lambda-example.core
-  (:gen-class :implements [com.amazonaws.services.lambda.runtime.RequestStreamHandler])
+    (:gen-class :implements [com.amazonaws.services.lambda.runtime.RequestStreamHandler])
   (:require [amazonica.aws.ec2 :as ec2]
+            [amazonica.aws.elasticloadbalancing :as elb]
             [clojure.data.json :as json]
             [clojure.edn :as edn]
             [clojure.java.io :as io]
@@ -8,45 +9,40 @@
             [clojure.walk :as walk]
             [org.httpkit.client :as http]))
 
-
-(defn get-instance-id-and-private-ips [instance]
+(defn get-health-status [instance]
   {:instance-id (:instance-id instance)
-   :private-ip (->> instance
-                     :network-interfaces
-                     (map :private-ip-address)
-                     clojure.string/join)})
+   :state (:state instance)})
 
-(defn get-instances-ec2 []
-  (->> (ec2/describe-instances
-        :filters [{:name "tag:env"
-              :values [(:env (edn/read-string (slurp (io/resource "config.edn"))))]}
-             {:name   "tag:type"
-              :values [(:type (edn/read-string (slurp (io/resource "config.edn"))))]}
-             {:name   "instance-state-name"
-              :values ["running" "pending"]}])
-       :reservations
-       (mapcat :instances)
-       (map get-instance-id-and-private-ips)))
+(defn get-elb-instances-status [elb-name]
+  (->>
+   (elb/describe-instance-health :load-balancer-name elb-name)
+   :instance-states
+   (map get-health-status )))
 
+(defn unhealthy-elb-instances [instances-status]
+  (->>
+   instances-status
+   (remove #(= (:state %) "InService"))
+   (map :instance-id)))
 
 (defn handle-event [event]
-  "Main handler of an AWS Event"
-  (let [instances (get-instances-ec2)]
-    (pprint "Hello I'm Lambda function")
-    (pprint instances)
-    {:message           (get-in event [:Records 0 :Sns :Message])
-     :instance-ids      (mapv :instance-id instances)}))
-
+  (let [instances (get-elb-instances-status
+                   (:load-balancer-name
+                    (edn/read-string (slurp (io/resource "config.edn")))))
+        unhealthy (unhealthy-elb-instances instances)]
+    (when (seq unhealthy)
+      (pprint "The next instances are unhealthy: ")
+      (pprint unhealthy)
+      (ec2/terminate-instances :instance-ids unhealthy))
+    {:message               (get-in event [:Records 0 :Sns :Message])
+     :elb-instance-ids      (mapv :instance-id instances)}))
 
 (defn -handleRequest [this is os context]
-  "Parser of input and genarator of JSON output"
   (let [w (io/writer os)]
-    (-> (io/reader is)
-        json/read
     (-> (io/reader is)
         json/read
         walk/keywordize-keys
         handle-event
         (json/write w))
-    (.flush w))))
+    (.flush w)))
 
